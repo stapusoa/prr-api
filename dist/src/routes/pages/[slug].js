192d7115da17d75d041f2cc6ff146b0d70e32438
Sara Tapusoa <79812543+stapusoa@users.noreply.github.com>
2025-08-29 13:36:39 -0600

    fix: readd api to git

--- dist/src/routes/pages/[slug].js
+++ dist/src/routes/pages/[slug].js
@@ -1,5 +1,5 @@
 // apps/api/src/routes/pages/[slug].ts
-import { getSanityClient } from "../../lib/cms/sanityClient";
+import { getSanityClient } from "../../lib/cms/sanityClient.js";
 export default async function handler(req, res) {
     const { slug } = req.params; // from [slug].ts
     try {
